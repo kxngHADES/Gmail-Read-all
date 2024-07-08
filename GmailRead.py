3c47839fac0be5e7b27b80192a552d9bc2648c54
kxngHADES <brandonmudau2@gmail.com>
2024-07-08 09:56:24 +0200

    ehh

--- GmailRead.py
+++ GmailRead.py
@@ -1,6 +1,6 @@
 import os
 import pickle
-import google.auth
+import time
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -9,15 +9,18 @@ from googleapiclient.discovery import build
 SCOPES = ['https://www.googleapis.com/auth/gmail.modify']
 
 def authenticate_gmail():
+    #bellow i made 2 lines of code a comment what happens is if you remove them the code will 
+    # ask you to relog back into your gmail when it executes again while the comment is on it will work on that same gmail/email all the time
+
     
-    if os.path.exists('token.pickle'):
-        os.remove('token.pickle')
+    # if os.path.exists('token.pickle'):
+    #     os.remove('token.pickle')
 
     creds = None
     if os.path.exists('token.pickle'):
         with open('token.pickle', 'rb') as token:
             creds = pickle.load(token)
-    
+
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -25,20 +28,19 @@ def authenticate_gmail():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentials.json', SCOPES)
             creds = flow.run_local_server(port=0)
-        
+
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
     return creds
 
 def mark_emails_as_read(service, except_emails):
-    
     query = 'is:unread'
     for email in except_emails:
         query += f' -from:{email}'
-    
+
     results = service.users().messages().list(userId='me', q=query).execute()
     messages = results.get('messages', [])
-    
+
     if not messages:
         print('No unread messages found.')
     else:
@@ -46,7 +48,7 @@ def mark_emails_as_read(service, except_emails):
             msg = service.users().messages().get(userId='me', id=message['id']).execute()
             msg_id = msg['id']
             msg_labels = msg['labelIds']
-            
+
             if 'UNREAD' in msg_labels:
                 msg_labels.remove('UNREAD')
                 service.users().messages().modify(userId='me', id=msg_id, body={'removeLabelIds': ['UNREAD']}).execute()
@@ -57,7 +59,12 @@ def main():
     service = build('gmail', 'v1', credentials=creds)
     
     except_emails = ['brandonmudau2@gmail.com', 'ndaedzomu101@gmail.com']  # Add all exception emails here
-    mark_emails_as_read(service, except_emails)
+    
+    
+    while True:
+        mark_emails_as_read(service, except_emails)
+        print("Waiting for 1 minute before checking again...")
+        time.sleep(60)  # Sleep for 60 seconds
 
 if __name__ == '__main__':
     main()
